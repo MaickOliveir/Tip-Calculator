ee98d10e856e4880de61919edb8ff6b74f22cb37
MaickOliveir <58923898+MaickOliveir@users.noreply.github.com>
2019-12-15 23:48:47 -0300

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,44 @@
+//Calculate Tip
+
+function calculateTip(){
+    var billAmt=document.getElementById("billamt").value;
+    var serviceQual=document.getElementById("serviceQual").value;
+    var numOfPeople=document.getElementById("peopleamt").value;
+
+//validate Input
+
+if (billAmt === "" || serviceQual==0){
+    alert("Please enter values");
+    return;
+}
+//check to see if this input is empty or less than or equal 1
+
+if (numOfPeople === "" || numOfPeople <=1){
+    numOfPeople=1;
+    document.getElementById("each").style.display = "none";
+} else {
+    document.getElementById("each").style.display = "block";
+}
+
+
+//calculate Tip
+
+var total =(billAmt * serviceQual) / numOfPeople;
+total =Math.round(total * 100) / 100;
+total=total.toFixed(2);
+document.getElementById("totalTip").style.display="block";
+document.getElementById("tip").innerHTML = total;
+}
+
+
+
+//Hide the tip amount on load
+
+document.getElementById("totalTip").style.display="none";
+document.getElementById("each").style.display = "none";
+
+//click to call the function
+
+document.getElementById("calculate").onclick = function() {
+    calculateTip();
+};
